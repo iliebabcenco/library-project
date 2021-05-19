2f5e94c0233a0341bf04af7b110c4588166f3763
FrederickMih <fredtimermih@gmail.com>
2021-05-19 11:02:50 +0100

    Add button to remove book from the libray

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 let library = [];
 
 
-function Book(author, title, numberOfPages) {
+function Book(author, title, numberOfPages, read = false) {
     this.author = author;
     this.title = title;
     this.numberOfPages = numberOfPages;
+    this.read =  read;
+
 }
 
 function addBookToLibrary() {
@@ -17,7 +19,7 @@ function addBookToLibrary() {
 function displayBooks() {
     for (let book of library) {
         let div = document.createElement('div');
-        div.innerHTML = "<div class=\'card-header\'>Book: "+book.title+"</div><div class='card-body'><h5 class='card-title'>Author: "+book.author+"</h5><p class='card-text'>Number of pages: "+book.numberOfPages+"</p></div>";
+        div.innerHTML = "<div class=\'card-header\'>Book: "+book.title+"</div><div class='card-body'><h5 class='card-title'>Author: "+book.author+"</h5><p class='card-text'>Number of pages: "+book.numberOfPages+"</p></div> <button onclick=" + removeBookFromLibrary(library.indexOf(book)) + ">Delete Book</button> <button onclick=" + removeBookFromLibrary(library.indexOf(book)) + ">Read Book</button>";
         div.style.maxWidth = '18rem';
         div.setAttribute('class', 'card text-dark bg-light mb-3');
         document.body.appendChild(div);
@@ -28,3 +30,10 @@ function displayForm() {
     let form = document.getElementsByClassName("myForm")[0];
     form.style.display = 'block';
 }
+
+function removeBookFromLibrary(id) {
+
+    library.splice(id, 1);
+
+}
+ displayBooks();
